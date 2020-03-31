b422b80f53f9f1770ba7849191dabc62fc7c13a4
rikidere <rorikidesu@gmail.com>
2020-03-31 23:27:56 +0200

    implemented AB :)

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # discord-sd-bots
 april fools
+https://discordapp.com/api/oauth2/authorize?client_id=694604245265154059&permissions=0&scope=bot  
+https://discordapp.com/api/oauth2/authorize?client_id=694607363457679403&permissions=0&scope=bot  
+https://discordapp.com/api/oauth2/authorize?client_id=694609713895309322&permissions=0&scope=bot  
+https://discordapp.com/api/oauth2/authorize?client_id=694610007484006511&permissions=0&scope=bot  
+https://discordapp.com/api/oauth2/authorize?client_id=694610184529641543&permissions=0&scope=bot  
--- bot.js
+++ bot.js
@@ -1,26 +1,34 @@
 const Discord = require('discord.js');
 const secrets = require('./secrets.json');
+let schedule = require('node-schedule');
 let clients = [];
 for (s in secrets){
     clients.push(new Discord.Client());
 }
 
-clients.forEach((c) => c.on('ready', () => {
-    console.log(`Logged in as ${c.user.tag}!`);
-  })
-);
 
-clients.forEach((c) => c.on('message', msg => {
-    if (msg.content === '!social distancing') {
-      msg.reply('Keep your distance and wash your hands :)');
-    }
-  })
-);
+clients.forEach((c) => {
+    
+    c.on('ready', () => {
+        console.log(`Logged in as ${c.user.tag}!`);
+        (c.user.username == "Alain Berset") && c.on('message', msg => {
+            (msg.content === '!social distancing') && msg.reply('Keep your distance and wash your hands :)');
+        }) && schedule.scheduleJob('26 * * * *', () => {
 
-clients.forEach((c) => c.on('error', e => {
-    console.error(e);
-    })
-);
+            let osc = c.guilds.resolve("124944592922476546");
+            let chan = osc.channels.resolve("694653358203732048");
+            chan.send('@everyone Keep your distance and wash your hands! :)');
+    
+        });
+    });
+
+    
+
+    c.on('error', e => {
+        console.error(e);
+    });
+});
+
 
 for (let i = 0; i < clients.length; i++){
     clients[i].login(secrets[i]);
